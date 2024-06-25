e6b93030c982e56a5fe32571f9552cf371e23ef0
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-25 20:39:52 +0530

    feat: api for get blos

---
+++ src/app/api/blog/getallblogs/route.ts
@@ -0,0 +1,14 @@
+import Blog from "@/models/blog.model";
+import dbConnection from "@/config/db";
+import { NextRequest, NextResponse } from "next/server";
+
+dbConnection();
+
+export const GET = async (req: NextRequest) => {
+    try {
+        const blogs = await Blog.find();
+        return NextResponse.json({ data: blogs }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+};
